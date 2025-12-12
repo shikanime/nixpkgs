8d263767be043795dff645613293070eac6f56d6
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-12 21:04:00 +0000

    ghstack: re-sync source fetch with tag version
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: Id68e4d607dbc2c9e3528b563dd1906236a6a6964

--- pkgs/by-name/gh/ghstack/package.nix
+++ pkgs/by-name/gh/ghstack/package.nix
@@ -12,8 +12,8 @@ python3.pkgs.buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "ezyang";
     repo = "ghstack";
-    rev = "fa7e7023d798aad6b115b88c5ad67ce88a4fc2a6";
-    hash = "sha256-Ywwjeupa8eE/vkrbl5SIbvQs53xaLnq9ieWRFwzmuuc=";
+    tag = version;
+    hash = "sha256-pLKwDezkwGrqYgP4WnIl5nAam6bMNO6BK+xbxhp7Aq8=";
   };
 
   postPatch = ''
